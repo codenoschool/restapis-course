877c749ab790d446596bb6aad25a529af433b45f
codenoschool <codenoschool@gmail.com>
2018-06-26 04:54:44 -0500

    SQL with Flask REST API.

--- Flask/data_persistence/sqldbs/finished/app.py
+++ Flask/data_persistence/sqldbs/finished/app.py
@@ -1,18 +1,12 @@
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
 
+# Do not forget to install these libraries in your virtualenv
+from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
 
 import os
 
-
-# This method to get an absolute path of a file works with all the operative systems.
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-"""
-If you're using MySQL or PostegreSQL you'll need to use another connector.
-Check the repository, video description or post where 
-you found this code for more information about it.
-"""
 DB_URI = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
 
 app = Flask(__name__)
@@ -23,50 +17,34 @@ db = SQLAlchemy(app)
 class Framework(db.Model):
     __tablename__ = "frameworks"
 
-    # The id will be unique, cannot be null, and auto-increase.
     id = db.Column(db.Integer, primary_key=True)
+    #name = db.Column(db.String(50), nullable=False, unique=True)
     name = db.Column(db.String(50))
 
 class FrameworkSchema(Schema):
     id = fields.Int()
     name = fields.Str()
-        
+
 @app.route("/")
 def index():
-    """
-    This route is irrelevant for the REST API, you can delete it if you want to.
-    """
 
     return "Hello World!"
 
-
-# GET METHOD
-
-@app.route("/api/frameworks/", methods=["GET"])
-def get_frameworks():
-    """
-    For this endpoint we'll be using Marshmallow to serialize the framework
-    objects that we got from the query, and then serialize the result again
-    but this time through the jsonify function to give a proper JSON response.
-
-    You can use this method for the rest of the methods which won't be using
-    marshmallow by default.
-    """
+@app.route("/api/frameworks/")
+def get_all_frameworks():
     frameworks = Framework.query.all()
-    frameworks_schema = FrameworkSchema(many=True)
-    result, errors = frameworks_schema.dump(frameworks)
+    framework_schema = FrameworkSchema(many=True)
+    results, errors = framework_schema.dump(frameworks)
+
+    return jsonify(results)
+
+@app.route("/api/frameworks/<string:name>", methods=["GET"])
+def get_one_framework_by_name(name):
+    framework = Framework.query.filter_by(name=name).first()
+    framework_schema = FrameworkSchema()
+    result, errors = framework_schema.dump(framework)
 
     return jsonify(result)
-
-@app.route("/api/frameworks/<string:name>")
-def get_framework_by_name(name):
-    framework = Framework.query.filter_by(name=name).first()
-    framework_dict = dict(id=framework.id, name=framework.name)
-
-    return jsonify(framework_dict)
-
-
-# POST METHOD
 
 @app.route("/api/frameworks/", methods=["POST"])
 def add_framework():
@@ -74,16 +52,16 @@ def add_framework():
     db.session.add(new_framework)
     db.session.commit()
 
-    framework_dict = dict(id=new_framework.id, name=new_framework.name)
+    framework_dict = {
+            "id": new_framework.id,
+            "name": new_framework.name
+            }
 
     return jsonify(framework_dict)
 
-
-# PUT METHOD
-
 @app.route("/api/frameworks/<int:id>", methods=["PUT"])
 def edit_framework(id):
-    framework = Framework.query.get(id)
+    framework = Framework.query.filter_by(id=id).first()
     framework.name = request.json["name"]
 
     db.session.commit()
@@ -92,32 +70,11 @@ def edit_framework(id):
 
     return jsonify(framework_dict)
 
-
-# DELETE METHOD
-
 @app.route("/api/frameworks/<int:id>", methods=["DELETE"])
 def delete_framework(id):
     framework = Framework.query.get(id)
-    
+
     db.session.delete(framework)
     db.session.commit()
 
     return jsonify({"message": "ok"})
-
-"""
-Since Flask 1.0 we are invited to use the following method to execute the app:
-
-GNU/Linux:
-    export FLASK_APP=app.py
-
-Windows OS:
-    CMD:
-        set FLASK_APP=app.py
-    Powershell:
-        $env:FLASK_APP = "app.py"
-
-Additionally you can set FLASK_ENV=development for Debug Mode
-
-You cand find more info about it here:
-http://flask.pocoo.org/docs/1.0/quickstart/#a-minimal-application
-"""
